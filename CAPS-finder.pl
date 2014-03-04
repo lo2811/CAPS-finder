e1d3a2cc712ba01c298fec5a95a66e4e69532abb
Mike Covington <mfcovington@gmail.com>
2014-03-03 16:26:42 -0800

    Define temp version of get_sequences sub

--- CAPS-finder.pl
+++ CAPS-finder.pl
@@ -10,8 +10,12 @@ use autodie;
 use feature 'say';
 use Data::Printer;
 
+my $id1 = 'WT';
+my $id2 = 'mutant';
+
 my $enzymes = restriction_enzymes();
 my $sites   = restriction_sites($enzymes);
+my $seqs    = get_sequences( $id1, $id2 );
 
 sub restriction_enzymes {
     return {
@@ -26,4 +30,12 @@ sub restriction_sites {
     my $enzymes = shift;
     my %sites = map { s|/||; $_ => 1 } values $enzymes;
     return [ keys %sites ];
+
+sub get_sequences {
+    my ( $id1, $id2 ) = @_;
+    return {
+        $id1 => 'cttctctagaggctttctcgcgaataagcGAATTCacgtgaagcgtcgatagccttcgat',
+        $id2 => 'cttctctagaggctttctcgcgaataagcGAtTTCacgtgaagcgtcgatagccttcgat',
+    }
+}
 }
